1f1853270c13b774b4e92c849c20e60d1749bea4
Camille BOUCHACOT <110158288+CamilleBouchacot@users.noreply.github.com>
2022-12-16 16:10:38 +0100

    ajout du format mobile

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,51 +1,36 @@
-import React from 'react'
-import '../Styles/main.css'
-import image from '../assets/banner.png'
-import Logo_footer from '../assets/Logo_footer.png'
-import { Link } from 'react-router-dom'
+import Banner from "../components/Banner";
+import aboutData from "../DATA/aboutData";
+import Collapse from "../components/Collapse";
+import imageBanner from "../assets/aboutbannerDesktop.png";
 
-export default function About() {
+const About = () => {
+  const content = (title) => {
+    switch (title) {
+      case "fiability":
+        return aboutData.fiability;
+      case "respect":
+        return aboutData.respect;
+      case "service":
+        return aboutData.service;
+      case "sécurity":
+        return aboutData.security;
+      default:
+        console.log("default");
+        break;
+    }
+  };
+
   return (
-    <>
-      <body>
-        <div>
-          <img className='img_About' src={image} alt='img' />
-        </div>
-        <nav>
-        <div className='About_bloc'>
-          <h4>Fiabilité</h4> 
-          <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements,
-            et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
-        </div>
-        <div className='About_bloc'>
-          <h4>Respect</h4>
-          <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
-            discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-        </div>
-        <div className='About_bloc'>
-          <h4>Service</h4>
-          <p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite.
-            N'hésitez pas à nous contacter si vous avez la moindre question.</p>
-        </div>
-        <div className='About_bloc'>
-          <h4>Responsabilité</h4>
-          <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,
-            chaque logement correspond aux critères de sécurité établis par nos services.
-            En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
-            que les standards sont bien respectés.
-            Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
-        </div>
-        </nav>
-      </body>
-      <footer>
-        <div className="Logo_footer">
-          <Link>
-            <img src={Logo_footer} alt='img' />
-          </Link>
-        </div>
-        <div>
-          <h5 className='footer_text'>© 2020 Kasa. All rights reserved</h5>
-        </div>
-      </footer>
-    </>
-)}
+    <div className="about">
+      <Banner image={imageBanner} />
+      <div className="about__dropdowns">
+        <Collapse title="fiabilité" content={content("fiability")} />
+        <Collapse title="respect" content={content("respect")} />
+        <Collapse title="service" content={content("service")} />
+        <Collapse title="sécurité" content={content("sécurity")} />
+      </div>
+    </div>
+  );
+};
+
+export default About;
